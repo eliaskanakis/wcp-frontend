97c33f0fe7936a0490df63e915aa0cff863c3f69
Ilias KANAKIS <ilias.kanakis.001@student.uni.lu>
2025-11-21 19:56:33 +0100

    34th

--- src/hooks/usePeerConnection.ts
+++ src/hooks/usePeerConnection.ts
@@ -96,7 +96,9 @@ export function usePeerConnection({
       };
 
       const pc = new RTCPeerConnection(PC_CONFIG);
-
+      pc.addTransceiver("video", { direction: "recvonly" });
+      pc.addTransceiver("audio", { direction: "recvonly" });
+      
       // ------------------ SAFARI → CHROME H264 BASELINE FIX ------------------
       try {
         const videoReceiverCaps = RTCRtpReceiver.getCapabilities("video")?.codecs ?? [];
